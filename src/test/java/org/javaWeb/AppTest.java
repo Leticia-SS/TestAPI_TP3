69b28a14fc233e5c08f70e387ac53e0e33e10413
Leticia-SS <leticia.saraiva013@gmail.com>
2025-06-08 19:58:03 -0300

    criando service para conexão com APIS eviltester

--- src/test/java/org/javaWeb/AppTest.java
+++ src/test/java/org/javaWeb/AppTest.java
@@ -4,26 +4,7 @@ import io.javalin.Javalin;
 
 public class AppTest {
     public static void main(String[] args) {
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create().start(8082);
 
-        app.before(ctx -> {
-           long inicio = System.currentTimeMillis();
-           ctx.attribute("inicio", inicio);
-           ctx.attribute("retorno", "Teste");
-            System.out.println("Antes da Rota: " + ctx.path());
-        });
-        app.after(ctx -> {
-            long inicio = ctx.attribute("inicio");
-            long end = System.currentTimeMillis();
-            long duracao = end - inicio;
-            System.out.println("Após a Rota: " + ctx.path() + " demorou: " + duracao + "ms");
-        });
-
-        app.get("/inicio", ctx -> {
-            System.out.println("Metodo Inicio");
-            ctx.result("Inicio");
-        });
-
-        app.start(8082);
     }
 }
